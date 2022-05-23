f33d7c3bdc4684ed8b3b9e67ab3941f922e44735
SanjayaDev <52363124+SanjayaDev@users.noreply.github.com>
2022-05-23 22:29:05 +0700

    InertiaJS Client
    + [ADD] Inertia JS Client
    + [ADD] Vue JS 3
    + [ADD] Setup Inertia JS
    + [ADD] Mixins check permissions

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,30 @@
-require('./bootstrap');
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
+
+import { InertiaProgress } from '@inertiajs/progress'
+
+createInertiaApp({
+  resolve: name => require(`./Pages/${name}`),
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .mixin({
+        methods: {
+          hasAnyPermission(permissions) {
+            let allPermissions = this.$page.props.auth.permissions;
+
+            return allPermissions.find(perm => permissions.includes(perm)) !== undefined;
+            // let hasPermission = false;
+            // permissions.forEach(function(item){
+            //   if(allPermissions[item]) hasPermission = true;     
+            // });
+
+            // return hasPermission;
+          }
+        }
+      })
+      .use(plugin)
+      .mount(el)
+  },
+});
+
+InertiaProgress.init();
