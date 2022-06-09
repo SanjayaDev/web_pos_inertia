5ed8e74c62ad80b88d9e409b185e8047e6f9a1b2
SanjayaDev <52363124+SanjayaDev@users.noreply.github.com>
2022-06-09 22:00:15 +0700

    Roles
    + [ADD] Login
    + [ADD] Forgot Password
    + [ADD] Dashboard
    + [ADD] Roles Management

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,13 +12,18 @@ createInertiaApp({
           hasAnyPermission(permissions) {
             let allPermissions = this.$page.props.auth.permissions;
 
-            return allPermissions.find(perm => permissions.includes(perm)) !== undefined;
-            // let hasPermission = false;
-            // permissions.forEach(function(item){
-            //   if(allPermissions[item]) hasPermission = true;     
-            // });
+            // return allPermissions.find(perm => permissions.includes(perm)) !== undefined;
+            let hasPermission = false;
+            permissions.forEach(function(item){
+              if(allPermissions[item]) hasPermission = true;     
+            });
 
-            // return hasPermission;
+            return hasPermission;
+          },
+
+          formatPrice(value) {
+            let val = (value/1).toFixed(0).replace('.', ',')
+            return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
           }
         }
       })
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Apps\DashboardController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Auth/Login');
+});
+
+Route::prefix("apps")->group(function() {
+    Route::group(["middleware" => ["auth"]], function() {
+        Route::get("/dashboard", DashboardController::class)->name("apps.dashboard");
+
+        Route::get("/permissions", \App\Http\Controllers\Apps\PermissionController::class)->name("apps.permissions.index")->middleware("permission:permissions.index");
+
+        Route::resource("/roles", \App\Http\Controllers\Apps\RoleController::class, ["as" => "apps"])
+        ->middleware("permission:roles.index|roles.create|roles.edit|roles.delete");
+    });
 });
